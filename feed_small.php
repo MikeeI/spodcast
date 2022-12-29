52ffbce89d1d7b7a0a7101184f0d2c2c69f38f88
MikeeI <MikeeI@users.noreply.github.com>
2022-12-29 23:36:10 +0100

    Update feed_small.php

--- feed_small.php
+++ feed_small.php
@@ -31,13 +31,14 @@
 
     $feed = new Itunes();
     $feed->registerAtomNamespace( );
+
+    $channel = $feed->addChannel();
     $channel->addLink(
         'atom:link',
         'https://raw.githubusercontent.com/MikeeI/spodcast/main/feed/4rOoJ6Egrf8K2IrywzwOMk.rss',
         'self', // the rel attribute
         'application/atom+xml'
     );
-    $channel = $feed->addChannel();
     $channel
         ->title($json_show["name"])
         ->subtitle($json_show["description"])
