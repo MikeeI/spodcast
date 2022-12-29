1d856739b759efd20c4c6c1b33c6f4face694027
MikeeI <MikeeI@users.noreply.github.com>
2022-12-29 23:58:11 +0100

    Update feed.php

--- feed.php
+++ feed.php
@@ -75,6 +75,7 @@
     }
 
     //echo $feed->toString();
+    $feed->prettyPrint( );
     file_put_contents("feed/4rOoJ6Egrf8K2IrywzwOMk_temp.rss", htmlspecialchars($feed->toString(),ENT_XML1, 'UTF-8'));
 
     function getEpisodes($spotify_show_id, $limit, $offset)
